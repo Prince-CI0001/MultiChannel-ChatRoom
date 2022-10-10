25deccdcfff883662b89fb78ff76f7b9a742f030
Prince-CI0001 <113011564+Prince-CI0001@users.noreply.github.com>
2022-10-10 22:31:08 +0530

    Delete get-user-name.pipe.spec.ts

--- src/app/get-user-name.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { GetUserNamePipe } from './get-user-name.pipe';
-
-describe('GetUserNamePipe', () => {
-  it('create an instance', () => {
-    const pipe = new GetUserNamePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
